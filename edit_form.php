eef94586db073f38f111d5ba7b9c43cec5a0aa71
Настя <a.goncharova@pomogaizer.ru>
2019-01-08 21:55:15 +0300

    Добавлена страница для проверки редактируемых данных

---
+++ edit_form.php
@@ -0,0 +1,27 @@
+<?php
+require_once "CRUD.php";
+require_once "registered_mail.php";
+$mail = new RegisteredMail();
+//var_dump($_POST);
+?>
+<h1>РЕДАКТИРОВАНИЕ ОТПРАВЛЕНИЯ</h1>
+<h3>Проверьте, пожалуйста, данные перед сохранением в систему.</h3>
+<p>Номер ID: <?= $_POST['number_id'] ?></p>
+<p>Тип отправления: <?php
+    $mail->typeMail = $_POST['type_mail'];
+    $type = $mail->getTitleTypeMail();
+    echo $type ?></p>
+<p>Отправитель: <?= $_POST['name_sender'] ?></p>
+<p>Адрес отправителя: <?= $_POST['address_sender'] ?></p>
+<p>Получатель: <?= $_POST['name_addressee'] ?></p>
+<p>Адрес Получателя: <?= $_POST['address_addressee'] ?></p>
+<form method="post" action="update_mail.php">
+    <input type="hidden" name="number_id" value="<?= $_POST['number_id'] ?>">
+    <input type="hidden" name="type_mail" value="<?= $_POST['type_mail'] ?>">
+    <input type="hidden" name="name_sender" value="<?= $_POST['name_sender'] ?>">
+    <input type="hidden" name="address_sender" value="<?= $_POST['address_sender'] ?>">
+    <input type="hidden" name="name_addressee" value="<?= $_POST['name_addressee']?> ">
+    <input type="hidden" name="address_addressee" value="<?= $_POST['address_addressee'] ?>">
+    <input type="hidden" name="status_mail" value="1"><br>
+    <input type="submit" name="save" value="Далее">
+</form>
