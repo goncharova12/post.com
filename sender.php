075d9d84262fa5971d653ab10ced1f87646e8cba
Настя <a.goncharova@pomogaizer.ru>
2019-01-09 23:09:47 +0300

    Добавлены методы замены имени и адреса отправителя

--- sender.php
+++ sender.php
@@ -58,4 +58,43 @@ class Sender extends CRUD
         return $result;
     }
 
+    /**
+     * Метод для изменения имени
+     * @param $oldNameSender string, имя которое нужно изменить
+     * @param $oldAddressSender string, старый адрес. Нужен для поиска строки, в которой нужно произвести замену
+     */
+    public function updateNameSender($oldNameSender, $oldAddressSender){
+        $this->newDataTable = $this->nameSender;
+        $this->nameTable = "sender";
+        $this->nameColumnTwo = "name_sender";
+        $this->nameColumn = "address_sender";
+        $this->dataTable = $oldAddressSender;
+        $this->oldDataTable = $oldNameSender;
+        $this->update();
+    }
+
+    /**
+     * метод для изменения адреса
+     * @param $oldNameSender string, старое имя. Нужно для поиска строки, по которой нужно произвести замену
+     * @param $oldAddressSender string, адрес, который нужно заменить.
+     */
+    public function updateAddress($oldNameSender, $oldAddressSender){
+        $this->newDataTable = $this->addressSender;
+        $this->nameTable = "sender";
+        $this->nameColumnTwo = "address_sender";
+        $this->nameColumn = "name_sender";
+        $this->dataTable = $oldNameSender;
+        $this->oldDataTable = $oldAddressSender;
+        $this->update();
+    }
+
+    /**
+     * метод для изменения имани и адреса получателя
+     * @param $oldNameSender string, старое имя
+     * @param $oldAddressSender string, старый адрес
+     */
+    public function updateNameAndAddressSender($oldNameSender, $oldAddressSender){
+        $this->updateNameSender($oldNameSender, $oldAddressSender);
+        $this->updateAddress($oldNameSender, $oldAddressSender);
+    }
 }
