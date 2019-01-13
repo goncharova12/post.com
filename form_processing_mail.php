111e7d14044a2744bc4c3ed86b3522919631949d
Настя <a.goncharova@pomogaizer.ru>
2019-01-13 19:07:26 +0300

    Изменено условие отправки данных для сохранения

--- form_processing_mail.php
+++ form_processing_mail.php
@@ -14,28 +14,76 @@ $mail = new RegisteredMail();
     <title>Прием РПО</title>
 </head>
 <body>
-<form method="post" action="verification_form.php">
-    <?php
-    $number = $numberId->findingAFreeID();
-    echo $number . "<input type='hidden' name='number_id' value='$number'><br>";
-    ?>
-    <input type="hidden" value="">
-    <select name="type_mail">
-        <option disabled selected>Выберите тип отправления</option>
+<?php if (!isset($_POST['action'])): ?>
+    <form method="post" action="">
         <?php
-        $mail->typeMail = "*";
-        $type = $mail->getTitleTypeMail2();
-        //        var_dump($type);
-        foreach ($type as $value) {
-            echo "<option value='{$value['number_type']}'>{$value['name_type']}</option>";
-        }
+        $number = $numberId->findingAFreeID();
+        echo $number . "<input type='hidden' name='number_id' value='$number'><br>";
         ?>
-    </select><br>
-    <input type="text" name="name_sender" placeholder="Отправитель"><br>
-    <input type="text" name="address_sender" placeholder="Адрес отправителя"><br>
-    <input type="text" name="name_addressee" placeholder="Адресат"><br>
-    <input type="text" name="address_addressee" placeholder="Адрес адресата"><br>
-    <input type="submit" name="action" value="далее"><br>
-</form>
+        <input type="hidden" value="">
+        <select name="type_mail">
+            <option disabled selected>Выберите тип отправления</option>
+            <?php
+            $mail->typeMail = "*";
+            $type = $mail->getTitleTypeMail2();
+            //        var_dump($type);
+            foreach ($type as $value) {
+                echo "<option value='{$value['number_type']}'>{$value['name_type']}</option>";
+            }
+            ?>
+        </select><br>
+        <input type="text" name="name_sender" placeholder="Отправитель"><br>
+        <input type="text" name="address_sender" placeholder="Адрес отправителя"><br>
+        <input type="text" name="name_addressee" placeholder="Адресат"><br>
+        <input type="text" name="address_addressee" placeholder="Адрес адресата"><br>
+        <input type="submit" name="action" value="далее"><br>
+    </form>
+<?php elseif (!empty($_POST['type_mail']) && !empty($_POST['name_sender']) && !empty($_POST['address_sender']) && !empty($_POST['name_addressee']) && !empty($_POST['address_addressee'])): ?>
+    <h3>Проверьте, пожалуйста, данные перед сохранением в систему.</h3>
+    <p>Номер ID: <?= $_POST['number_id'] ?></p>
+    <p>Тип отправления: <?php
+        $mail->typeMail = $_POST['type_mail'];
+        $type = $mail->getTitleTypeMail();
+        echo $type ?></p>
+    <p>Отправитель: <?= $_POST['name_sender'] ?></p>
+    <p>Адрес отправителя: <?= $_POST['address_sender'] ?></p>
+    <p>Получатель: <?= $_POST['name_addressee'] ?></p>
+    <p>Адрес Получателя: <?= $_POST['address_addressee'] ?></p>
+    <form method="post" action="saving_mail.php">
+        <input type="hidden" name="number_id" value="<?= $_POST['number_id'] ?>">
+        <input type="hidden" name="type_mail" value="<?= $_POST['type_mail'] ?>">
+        <input type="hidden" name="name_sender" value="<?= $_POST['name_sender'] ?>">
+        <input type="hidden" name="address_sender" value="<?= $_POST['address_sender'] ?>">
+        <input type="hidden" name="name_addressee" value="<?= $_POST['name_addressee'] ?> ">
+        <input type="hidden" name="address_addressee" value="<?= $_POST['address_addressee'] ?>">
+        <input type="hidden" name="status_mail" value="1"><br>
+        <input type="submit" name="save" value="Далее">
+    </form>
+<?php else: ?>
+    <p>Введите все данные</p>
+    <form method="post" action="">
+        <?php
+        $number = $numberId->findingAFreeID();
+        echo $number . "<input type='hidden' name='number_id' value='$number'><br>";
+        ?>
+        <input type="hidden" value="">
+        <select name="type_mail">
+            <option disabled selected>Выберите тип отправления</option>
+            <?php
+            $mail->typeMail = "*";
+            $type = $mail->getTitleTypeMail2();
+            //        var_dump($type);
+            foreach ($type as $value) {
+                echo "<option value='{$value['number_type']}'>{$value['name_type']}</option>";
+            }
+            ?>
+        </select><br>
+        <input type="text" name="name_sender" placeholder="Отправитель"><br>
+        <input type="text" name="address_sender" placeholder="Адрес отправителя"><br>
+        <input type="text" name="name_addressee" placeholder="Адресат"><br>
+        <input type="text" name="address_addressee" placeholder="Адрес адресата"><br>
+        <input type="submit" name="action" value="далее"><br>
+    </form>
+<?php endif; ?>
 </body>
 </html>
--- form_update_mail.php
+++ form_update_mail.php
@@ -24,8 +24,8 @@ $mailInfo = $mail->getDesiredMail($array);
     <title>Редактирование РПО</title>
 </head>
 <body>
-<form method="post" action="edit_form.php">
-
+<?php if(!isset($_POST['action'])):?>
+<form method="post" action="form_update_mail.php">
     <?php
     $number = $mailInfo['0']['number_id'];
     echo $number . "<input type='hidden' name='number_id' value='$number'><br>";
@@ -54,5 +54,63 @@ $mailInfo = $mail->getDesiredMail($array);
     <input type="hidden" name="old_address_addressee" value="<?=$mailInfo['0']['address']?>"><br>
     <input type="submit" name="action" value="далее"><br>
 </form>
+
+<?php elseif (!empty($_POST['number_id']) && !empty($_POST['type_mail']) && !empty($_POST['name_sender']) && !empty($_POST['address_sender']) && !empty($_POST['name_addressee']) && !empty($_POST['address_addressee'])):?>
+<h1>РЕДАКТИРОВАНИЕ ОТПРАВЛЕНИЯ</h1>
+<h3>Проверьте, пожалуйста, данные перед сохранением в систему.</h3>
+<p>Номер ID: <?= $_POST['number_id'] ?></p>
+<p>Тип отправления: <?php
+    $mail->typeMail = $_POST['type_mail'];
+    $type = $mail->getTitleTypeMail();
+    echo $type ?></p>
+<p>Отправитель: <?= $_POST['name_sender'] ?></p>
+<p>Адрес отправителя: <?= $_POST['address_sender'] ?></p>
+<p>Получатель: <?= $_POST['name_addressee'] ?></p>
+<p>Адрес Получателя: <?= $_POST['address_addressee'] ?></p>
+<form method="post" action="update_mail.php">
+    <input type="hidden" name="number_id" value="<?= $_POST['number_id'] ?>">
+    <input type="hidden" name="type_mail" value="<?= $_POST['type_mail'] ?>">
+    <input type="hidden" name="name_sender" value="<?= $_POST['name_sender'] ?>">
+    <input type="hidden" name="old_name_sender" value="<?=$_POST['old_name_sender']?>">
+    <input type="hidden" name="address_sender" value="<?= $_POST['address_sender'] ?>">
+    <input type="hidden" name="old_address_sender" value="<?=$_POST['old_address_sender']?>">
+    <input type="hidden" name="name_addressee" value="<?= $_POST['name_addressee']?> ">
+    <input type="hidden" name="old_name_addressee" value="<?= $_POST['old_name_addressee']?>">
+    <input type="hidden" name="address_addressee" value="<?= $_POST['address_addressee'] ?>">
+    <input type="hidden" name="old_address_addressee" value="<?=$_POST['old_address_addressee']?>">
+    <input type="hidden" name="status_mail" value="1"><br>
+    <input type="submit" name="update" value="Далее">
+</form>
+<?php else: ?>
+<form method="post" action="form_update_mail.php">
+    <?php
+    $number = $mailInfo['0']['number_id'];
+    echo $number . "<input type='hidden' name='number_id' value='$number'><br>";
+    ?>
+    <select name="type_mail">
+        <?php
+        $mail->typeMail = "*";
+        $type = $mail->getTitleTypeMail2();
+        //        var_dump($type);
+        foreach ($type as $value) {
+            if ($value['name_type'] == $mailInfo['0']['name_type']) {
+                echo "<option value='{$value['number_type']}' selected>{$value['name_type']}</option>";
+            } else {
+                echo "<option value='{$value['number_type']}'>{$value['name_type']}</option>";
+            }
+        }
+        ?>
+    </select><br>
+    <input type="text" name="name_sender" value="<?=$mailInfo['0']['name_sender']?>">
+    <input type="hidden" name="old_name_sender" value="<?=$mailInfo['0']['name_sender']?>"><br>
+    <input type="text" name="address_sender" value="<?=$mailInfo['0']['address_sender']?>">
+    <input type="hidden" name="old_address_sender" value="<?=$mailInfo['0']['address_sender']?>"><br>
+    <input type="text" name="name_addressee" value="<?=$mailInfo['0']['name_addressee']?>">
+    <input type="hidden" name="old_name_addressee" value="<?=$mailInfo['0']['name_addressee']?>"><br>
+    <input type="text" name="address_addressee" value="<?=$mailInfo['0']['address']?>">
+    <input type="hidden" name="old_address_addressee" value="<?=$mailInfo['0']['address']?>"><br>
+    <input type="submit" name="action" value="далее"><br>
+</form>
+<?php endif;?>
 </body>
 </html>
