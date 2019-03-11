d9e0cc24102521a533479b514befe869f6d65239
Анастасия Гончарова <gon4arowa12@yandex.ru>
2019-03-11 12:24:39 +0300

    Изменена логика отображения файлов

--- controllers/postalServices_controller.php
+++ controllers/postalServices_controller.php
@@ -29,10 +29,32 @@ class PostalServicesController extends Controller
         $numberId = new NumberId();
         $mail = new RegisteredMail();
         $mail->typeMail = $_POST['type_mail'];
-        $type = $mail->getTitleTypeMail2('*');
+        $type = $mail->getTitleTypeMail();
+        $mail->typeMail = $_POST['type_mail'];
+        $types = $mail->getTitleTypeMail2('*');
         $array = $_POST;
-        $result = compact( 'type', 'array', $mail->typeMail);
+        $result = compact('type', 'array', 'types');
         $this->view->page = "postalServices/processing_mail/verification_page.php";
+        $this->view->render($result);
+    }
+
+    public function actionSavingMail()
+    {
+        $numberId = new NumberId();
+        $mail = new RegisteredMail();
+        $sender = new Sender();
+        $addressee = new Addressee();
+
+        $mail->numberId = trim(urldecode(htmlspecialchars($_POST['number_id'])));
+        $mail->typeMail = trim(urldecode(htmlspecialchars($_POST['type_mail'])));
+        $sender->nameSender = trim(urldecode(htmlspecialchars($_POST['name_sender'])));
+        $sender->addressSender = trim(urldecode(htmlspecialchars($_POST['address_sender'])));
+        $addressee->nameAddressee = trim(urldecode(htmlspecialchars($_POST['name_addressee'])));
+        $addressee->addressAddressee = trim(urldecode(htmlspecialchars($_POST['address_addressee'])));
+        $mail->statusMail = trim(urldecode(htmlspecialchars($_POST['status_mail'])));
+        $mail->createMail($numberId, $addressee, $sender);
+        $result = ['text' => 'Отправление сохранено'];
+        $this->view->page = "postalServices/processing_mail/saving_mail.php";
         $this->view->render($result);
     }
 
